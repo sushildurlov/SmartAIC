1ca24cf3d93f5b1e067d54a67cb8929a8a3b2962
sushildurlov <sushil.161114@ncit.edu.np>
2023-01-16 17:43:45 +0900

    configure CMakeLists file

---
+++ build/CMakeFiles/CT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CT.dir/src/CT/Snsrctlapp.c.o"
+  "CMakeFiles/CT.dir/src/CT/ctdata.c.o"
+  "CT"
+  "CT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
